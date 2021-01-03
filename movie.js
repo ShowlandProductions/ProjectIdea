c34310a556ed4a72af6a9a7f663d90bb7524cb2c
ShowlandProductions <35757926+ShowlandProductions@users.noreply.github.com>
2021-01-03 15:54:54 -0800

    Add files via upload

--- movie.js
+++ movie.js
@@ -1,15 +1,13 @@
-	var movie = $(this).attr("data-name");
-	var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+var movie = $(this).attr("data-name");
+var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
 
-	$.ajax({
-	  url: queryURL,
-	  method: "GET"
-	}).then(function(response) {
-	  $("#movies-view").text(JSON.stringify(response));
-	});
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  $("#movies-view").text(JSON.stringify(response));
+});
 
-	alert(movieName)
-  }
-
-
+alert(movieName)
+}
 
