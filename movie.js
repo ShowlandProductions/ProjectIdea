8807dacb2ef17a5686cac6215779f20baae62be0
ShowlandProductions <35757926+ShowlandProductions@users.noreply.github.com>
2021-01-04 13:55:40 -0800

    Add files via upload

--- movie.js
+++ movie.js
@@ -1,13 +1,33 @@
-var movie = $(this).attr("data-name");
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+$(movieForm).on("submit", searchMovie)
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  $("#movies-view").text(JSON.stringify(response));
-});
-
-alert(movieName)
+function searchMovie(e) {
+	e.preventDefault()
+	$(movieDisplay).removeClass('hide')
+	
+	const title_value = $(title).val()
+	const type_value = $(type).val()
+	const plot_value = $(plot).val()
+	const year_value = $(year).val()
+	
+	let baseURL = "https://www.omdbapi.com/"
+	let query = "?t=" + title_value
+	query += "&type=" + type_value
+	query += "&plot=" + plot_value
+	
+	if (year_value) query += "&y=" + year_value
+	
+	query += "&apikey=trilogy"
+	
+	const queryURL = baseURL + query
+	
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function (response) {
+		console.log(response)
+		$(movie_title).text(response.Title)
+		$(movie_genre).text(response.Genre)
+		$(movie_plot).text(response.Plot)
+		$(movie_poster).attr('src', response.Poster)
+	});
 }
-
